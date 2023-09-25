e3a4a17ff84d3efd3beebf727f4aac530fa75f88
khalilb2000 <khalilb2000@gmail.com>
2023-09-25 16:50:00 -0500

    adding styling

--- questions.js
+++ questions.js
@@ -56,52 +56,54 @@ let questions = [
  
 ];
 
-let currentQuestion = 0
-let score = 0
+let currentQuestion = 0;
+let score = 0;
 
 const questionText = document.getElementById("question-text");
 const optionsContainer = document.getElementById("options-container");
 const nextButton = document.getElementById("next-button");
 const resultsContainer = document.getElementById("results-container");
 
-function Loadquestions(){
-const question = questions[currentQuestion]
-questionText.textContent = question.questions;
-optionsContainer.innerHTML = '';
+function loadQuestion() {
+  const question = questions[currentQuestion];
+  questionText.textContent = question.question;
+  optionsContainer.innerHTML = '';
 
-questions.options.forEach((option, index) => {
+  question.options.forEach((option, index) => {
     const optionElement = document.createElement("div");
     optionElement.textContent = option;
-    optionElement.addEventListener("click",() => checkAnswer(index));
+    optionElement.addEventListener("click", () => checkAnswer(index));
     optionsContainer.appendChild(optionElement);
+  });
+}
+
+function checkAnswer(selectedIndex) {
+  const question = questions[currentQuestion];
+  if (question.options[selectedIndex] === question.answer) {
+    score++;
+  }
+
+  currentQuestion++;
+
+  if (currentQuestion < questions.length) {
+    loadQuestion();
+  } else {
+    showResult();
+  }
+}
+
+function showResult() {
+  questionText.textContent = "Quiz finished!";
+  optionsContainer.innerHTML = '';
+  nextButton.style.display = 'none';
+  resultsContainer.textContent = `Your score: ${score} out of ${questions.length}`;
+}
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestion < questions.length) {
+    loadQuestion();
+  }
 });
 
-}
-
-function checkAnswer(selectedIndex){
-    const question = question[currentQuestion];
-    if(question.options[selectedIndex]=== question.answer){
-        score++;
-    }
-}
-
-currentQuestion++;
-
-if(currentQuestion < questions.length){
-    loadQuestion();
-} else {
-    showResult();
-}
-
-function showResult(){
-    questionText.textContent = "Quiz finished!";
-    optionsContainer.innerHTML = '';
-    nextButton.style.display = 'none';
-    resultsContainer.textContent = `Your score: ${score} out of ${question.lengths}`;
-}
-
-nextButton.addEventListener("click",() => {
-    if (currentQuestion < questions.length){
-        loadQuestion();
-    }
-})
+// Start the quiz by loading the first question
+loadQuestion();

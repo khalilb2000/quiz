ac19d48145d6c0e57ff2fc9ab49a602b5d97b4f0
khalilb2000 <khalilb2000@gmail.com>
2023-09-25 16:28:41 -0500

    redid the functions for the quiz

---
+++ script.js
@@ -0,0 +1,45 @@
+let currentQuestion = 0
+let score = 0
+
+const questionText = document.getElementById("question-text");
+const optionsContainer = document.getElementById("options-container");
+const nextButton = document.getElementById("next-button");
+const resultsContainer = document.getElementById("results-container");
+
+function Loadquestions(){
+const question = questions[currentQuestion]
+questionText.textContent = question.questions;
+optionsContainer.innerHTML = '';
+
+questions.options.forEach((option, index) => {
+    const optionElement = document.createElement("div");
+    optionElement.textContent = option;
+    optionElement.addEventListener("click",() => checkAnswer(index));
+    optionsContainer.appendChild(optionElement);
+});
+
+}
+
+function checkAnswer(selectedIndex){
+    const question = question[currentQuestion];
+    if(question.options[selectedIndex]=== question.answer){
+        score++;
+    }
+}
+
+currentQuestion++;
+
+if(currentQuestion < questions.length){
+    loadQuestion();
+} else {
+    showResult();
+}
+
+function showResult(){
+    questionText.textContent = "Quiz finished!";
+    optionsContainer.innerHTML = '';
+    nextButton.style.display = 'none';
+    resultsContainer.textContent = `Your score: ${score} out of ${question.lengths}`;
+}
+
+nextButton.addEventListener("click")
